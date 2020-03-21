ac4ac854955458abb8f9947bc185abdbfba4015a
Yahya Chahine <chahineyahya@pm.me>
2020-03-21 23:30:41 +0100

    Corrected Router Link Name

--- components/NavBar.js
+++ components/NavBar.js
@@ -42,7 +42,7 @@ function NavBar() {
           </Link>
 
           <Link href="/informations">
-            <a href="" className="block hover:underline md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-lg text-gray-500 hover:text-gray-900">
+            <a href="/informations" className="block hover:underline md:inline-block mt-4 md:mt-0 md:ml-6 no-underline text-lg text-gray-500 hover:text-gray-900">
               Informations
             </a>
           </Link>
